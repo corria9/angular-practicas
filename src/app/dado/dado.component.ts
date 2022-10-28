d730b874aa7073a428382b00b92555293d849707
Juan Armando Coria Fierros <juan.coria@neoris.com>
2022-10-28 11:39:43 -0500

    Se sube componente Dado Random @Input

--- src/app/dado/dado.component.ts
+++ src/app/dado/dado.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-dado',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dado.component.css']
 })
 export class DadoComponent implements OnInit {
+  @Input()
   valor: number = 1;
   constructor() { }
 
